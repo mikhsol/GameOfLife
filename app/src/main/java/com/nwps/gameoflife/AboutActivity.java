4c143f56bae4860db47f085a694802586e6d3ff6
Mikhail <miksolodovnikov@gmail.com>
2017-02-05 19:02:15 +0500

    Add few activities and layouts.

---
+++ app/src/main/java/com/nwps/gameoflife/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.nwps.gameoflife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
