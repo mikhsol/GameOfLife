b268924e3591468d86412e8ceee1fda4a35a92d7
Mikhail <miksolodovnikov@gmail.com>
2017-02-05 14:56:41 +0500

    The Game Of Life application inti commit.

---
+++ app/src/main/java/com/nwps/gameoflife/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nwps.gameoflife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nwps.game_of_life.gameoflife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
