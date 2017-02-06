c73dc4210fae673ed77236ed8f691c7de1abd704
Mikhail <miksolodovnikov@gmail.com>
2017-02-06 17:40:23 +0500

    Remove unnecesary elements from main activity.

--- app/src/main/java/com/nwps/gameoflife/MainActivity.java
+++ app/src/main/java/com/nwps/gameoflife/MainActivity.java
@@ -1,8 +1,11 @@
 package com.nwps.gameoflife;
 
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
 
         View aboutButton = findViewById(R.id.about_button);
         aboutButton.setOnClickListener(this);
@@ -26,6 +31,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.new_game_button:
                 startActivity(new Intent(this, GridActivity.class));
+                break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return false;
     }
 }
